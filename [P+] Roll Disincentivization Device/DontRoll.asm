7476e061135eb4c814c3222f559daa7398d79db3
QuickLava <fastmovingvolcanomatter@gmail.com>
2024-03-28 16:34:31 -0700

    DontRoll Can Now Disable Rolls/Spotdodges

--- [P+] Roll Disincentivization Device/DontRoll.asm
+++ [P+] Roll Disincentivization Device/DontRoll.asm
@@ -1,16 +1,21 @@
 ##################################################################################################
-Roll Disincentivization Device v1.0.0 [QuickLava]
+Roll Disincentivization Device v1.0.1 [QuickLava]
 # Replace "targetActionID" value with the Action ID you want Rolling/Spotdodging to trigger!
-# Full List of Actions on Fudge's Wiki: https://brawlre.github.io/public/
-# Notable Punishment Values: 0x56 = Shieldbreak, 0x5C =  Sleep, 0x89 =  Trip, 0xBD = Instant Death
+# Full list of actions on Fudge's Wiki: https://brawlre.github.io/public/
+# To simply disable rolls and spotdodge, set ActionID to 0xFFFF!
+# Other Notable Values: 0x56 = Shieldbreak, 0x5C =  Sleep, 0x89 =  Trip, 0xBD = Instant Death
 ##################################################################################################
 .alias targetActionID = 0x56
-HOOK @ $8077F9DC             # [in symbol "changeStatus/[soStatusModuleImpl]/so_status_module_impl.o" @ 0x8077F9C4]
+HOOK @ $8077F9C4             # [in symbol "changeStatus/[soStatusModuleImpl]/so_status_module_impl.o" @ 0x8077F9C4]
 {
-  mr r29, r3
   cmplwi r4, 0x1E            # \
-  blt %END%                  # |
+  blt+ exit                  # |
   cmplwi r4, 0x20            # | If target action is between 0x1E and 0x20 (ie. is a Spotdodge or Roll)...
-  bgt %END%                  # /
+  bgt+ exit                  # /
   li r4, targetActionID      # ... target specified target action instead!
+  cmpwi r4, 0xFFFF           # \ 
+  bne exit                   # | If we're using the "disable" value though...
+  blr                        # / ... simply return to avoid changing status at all.
+exit:
+  stwu r1, -0x40(r1)         # Restore Original Instruction
 }
